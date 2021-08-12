7dfff634f7ce06a48e77915e4f9a2ee5c566d157
KahinaP <kahina.pellerin@gmail.com>
2021-08-12 14:51:04 +0200

    dernier update

--- html/docjs.js
+++ html/docjs.js
@@ -7,17 +7,15 @@ let open = false;
 // function
 let openNav = ()=>{
     if (open === false){
-        //recuperer dans une variable js l'elemt nav
-        //modifier la proporiété style de nav à 200px
+   
         nav.style.width = "200px";
-        // recuperer dans une variable js l'element #shadow
-        // modifier la propriété display à block
+   
         shadow.style.display = "block";
         open = true;
     }else{
-        //remettre width 0px sur la balise nav
+        
         nav.style.width = "0";
-        //remettre un display none sur l'elmt #shadow
+        
         shadow.style.display = "none";
         open = false;
     }
